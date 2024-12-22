a868b842ea59ebecbe880859d62667ba078f8fa5
Chandramouli-Pyla <61827145+Chandramouli-Pyla@users.noreply.github.com>
2024-12-21 19:57:14 -0600

    Add files via upload

---
+++ Day2/Employee.java
@@ -0,0 +1,21 @@
+class Employee {
+  public static void main(String[] args) {
+    // Employee data
+    String employeeName = "John Doe";
+    int employeeID = 1231234;
+    int employeeAge = 23;
+    Long employeeNum = 2175120038L;
+    float employeeSalary = 6475.25f;
+    double employeeSalInRsPerYear =  6581444.1d;
+    boolean isLegal = true;
+    
+    // Print variables
+    System.out.println("Employee name: " + employeeName);
+    System.out.println("Employee id: " + employeeID);
+    System.out.println("Employee age: " + employeeAge);
+     System.out.println("Employee Number: " + employeeNum);
+    System.out.println("Employee Salary: " + employeeSalary);
+    System.out.println("Employee Salary In Rupess per Year: " + employeeSalInRsPerYear);
+    System.out.println("Is legal Employee or not: "+isLegal);
+  }
+}
